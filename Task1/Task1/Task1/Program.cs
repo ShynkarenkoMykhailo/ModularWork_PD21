9c801544ca2188cbb57b61e50c707f69f3d5e87d
Mykhailo <st9273155@stud.duikt.edu.ua>
2026-03-25 17:35:11 +0200

    Add project files.

---
+++ Task1/Task1/Task1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace Task1_Delegates
+{
+    public delegate string TextOperation(string text);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText("textPD21.txt", "Привіт! Це тестовий рядок для перевірки.");
+
+            File.WriteAllText("resultPD21.txt", "");
+
+            ProcessFile(ToUpperCase);
+            ProcessFile(CountCharacters);
+            ProcessFile(CountWords);
+
+            Console.WriteLine("Завдання 1 виконано. Перевірте файл resultPD21.txt");
+            Console.ReadLine();
+        }
+
+        static string ToUpperCase(string text)
+        {
+            return text.ToUpper();
+        }
+                 
+        static string CountCharacters(string text)
+        {
+            int count = text.Length;
+            return "Кількість символів: " + count;
+        }
+
+        static string CountWords(string text)
+        {
+            string[] words = text.Split(new char[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            int count = words.Length;
+            return "Кількість слів: " + count;
+        }
+
+        static void ProcessFile(TextOperation operation)
+        {
+            string inputFile = "textPD21.txt";
+            string outputFile = "resultPD21.txt";
+
+            string text = File.ReadAllText(inputFile);
+
+            string result = operation(text);
+
+            File.AppendAllText(outputFile, result + "\n");
+        }
+    }
+}
